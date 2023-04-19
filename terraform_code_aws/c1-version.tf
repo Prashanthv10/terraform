f7ab08985796edd578d699383fefcef6acb3c6fb
prashanth <prashant301k@gmail.com>
2023-04-19 21:30:43 +0530

    added providers

--- terraform_code_aws/c1-version.tf
+++ terraform_code_aws/c1-version.tf
@@ -1,9 +1,12 @@
 terraform {
-  required_version = "~>0.14.3"
+  // required_version = "~>0.14.3"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "~3.21"
     }
   }
+}
+provider "aws" {
+  region = "southeast-2"
 }
