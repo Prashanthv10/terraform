7d6253c1460d1862431b317f1443675a3e9f0a32
prashanth <prashant301k@gmail.com>
2023-04-24 19:11:37 +0530

    removed datasource

--- terraform_code_aws/c6_output.tf
+++ terraform_code_aws/c6_output.tf
@@ -1,9 +1,10 @@
 output "public_ip" {
-    value = "aws_instance.ec2.public_ip"
+  value = "aws_instance.ec2.public_ip"
 }
 output "public_dns" {
-    value = "aws_instance.ec2.public_dns"
+  value = "aws_instance.ec2.public_dns"
 }
+/*
 output "name" {
     value = "aws_instance.ec2.aws_ami.id"
 }
