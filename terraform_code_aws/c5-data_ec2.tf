6c89d439fc0c721ffa634b493326f1b2241ece07
prashanth <prashant301k@gmail.com>
2023-04-25 14:54:14 +0530

    tags name added

--- terraform_code_aws/c5-data_ec2.tf
+++ terraform_code_aws/c5-data_ec2.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "ec2" {
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.vpc-web.id, aws_security_group.vpc-ssh.id ]
   tags = {
-    "name" = "ec2p"
+    "Name" = "ec2p"
   }
 }
