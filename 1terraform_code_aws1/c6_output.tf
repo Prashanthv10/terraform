b18dbaf92ffb2fbd607e6b8c378cce00f028fad5
prashanth <prashant301k@gmail.com>
2023-05-02 12:56:38 +0530

    tfvars

--- 1terraform_code_aws1/c6_output.tf
+++ 1terraform_code_aws1/c6_output.tf
@@ -1,13 +1,20 @@
 output "instance_publicip" {
-    description = "ec2publicip"
-    value = "aws_instance.ec2.public_ip"
+  description = "ec2publicip"
+  value = "aws_instance.ec2.public_ip"
 }
 output "instance_publicdns" {
-    description = "ec2dns"
-    value = "aws_instance.ec2.public_dns"
+  description = "ec2dns"
+  value = "aws_instance.ec2.public_dns"
+  value = toset(aws_instance.ec2.public_dns)
 }
 /*
 output "name" {
     value = "aws_instance.ec2.aws_ami.id"
 }
 */
+
+# Output Legacy Splat Operator (latest) - Returns the List
+output "legacy_splat_instance_publicdns" {
+  description = "Legacy Splat Expression"
+  value = aws_instance.ec2.[*].public_dns
+}  
