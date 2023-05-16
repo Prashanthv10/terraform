cda3ae166d7a961c45e8f55cc95f47397b687393
prashanth <prashant301k@gmail.com>
2023-05-16 17:54:32 +0530

    ec2 instances on differnt subnets

--- loadbalancer/c10-03-aplcn-lb-output.tf
+++ loadbalancer/c10-03-aplcn-lb-output.tf
@@ -1,24 +1,24 @@
-output "target_group_name" {
-    value = module.alb.target_group_name
-}
-output "loadbalancer-lb_arn" {
-    value = module.alb.this_lb_arn
-}
-output "loadbalancer-lb_dns_name" {
-    value = module.alb.this_lb_dns_name
-}
-output "lb_zone_id" {
-    value = module.alb.this_lb_id
-}
-output "target_group_arn_suffixes" {
-  description = "ARN suffixes of our target groups - can be used with CloudWatch."
-  value       = module.alb.target_group_arn_suffixes
-}
-output "target_group_arns" {
-  description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
-  value       = module.alb.target_group_arns
-}
-output "this_lb_arn_suffix" {
-  description = "ARN suffix of our load balancer - can be used with CloudWatch."
-  value       = module.alb.this_lb_arn_suffix
-}
+# output "target_group_name" {
+#     value = module.alb.target_group_name
+# }
+# output "loadbalancer-lb_arn" {
+#     value = module.alb.this_lb_arn
+# }
+# output "loadbalancer-lb_dns_name" {
+#     value = module.alb.this_lb_dns_name
+# }
+# output "lb_zone_id" {
+#     value = module.alb.this_lb_id
+# }
+# output "target_group_arn_suffixes" {
+#   description = "ARN suffixes of our target groups - can be used with CloudWatch."
+#   value       = module.alb.target_group_arn_suffixes
+# }
+# output "target_group_arns" {
+#   description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
+#   value       = module.alb.target_group_arns
+# }
+# output "this_lb_arn_suffix" {
+#   description = "ARN suffix of our load balancer - can be used with CloudWatch."
+#   value       = module.alb.this_lb_arn_suffix
+# }
