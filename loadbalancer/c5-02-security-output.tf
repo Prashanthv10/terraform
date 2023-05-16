3ce9e3296813a7a620c31529b1ef1390cdeef801
prashanth <prashant301k@gmail.com>
2023-05-16 18:11:40 +0530

    ec2 instances on differnt subnets

--- loadbalancer/c5-02-security-output.tf
+++ loadbalancer/c5-02-security-output.tf
@@ -1,12 +1,12 @@
 output "public_security_group_id" {
-    value = module.public-bastion-sg.security_group_id
+    value = module.public-bastion-sg.this_security_group_id
 }
 output "private_sg_grp_id" {
-    value = module.private-sg.security_group_id
+    value = module.private-sg.this_security_group_id
 }
 output "public_sg_grp_name" {
-    value = module.public-bastion-sg.security.group.name
+    value = module.public-bastion-sg.this_security.group.name
 }
 output "private_sg_grp_name" {
-    value = module.private-sg.security.group.name
+    value = module.private-sg.this_security.group.name
 }
