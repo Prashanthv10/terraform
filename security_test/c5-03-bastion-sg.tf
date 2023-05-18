9ff34847fba7881f19a1a0f063b0db226963d12d
prashanth <prashant301k@gmail.com>
2023-05-18 21:39:10 +0530

    security grp test

--- security_test/c5-03-bastion-sg.tf
+++ security_test/c5-03-bastion-sg.tf
@@ -3,7 +3,7 @@ module "public-bastion-sg" {
     # version = "4.0.0"
     # insert the 2 required variables here
 
-    # name = "public-bastion-sg"
+    name = "public-bastion-sg"
     vpc_id = module.vpc.vpc_id
 
     # tags = local.common_tags
--- security_test/c5-04-private-securitygrp.tf
+++ security_test/c5-04-private-securitygrp.tf
@@ -3,7 +3,7 @@ module "private-sg" {
   # version = "4.0.0"
   # insert the 2 required variables here
  
-#  name = "private-sg"
+  name = "private-sg"
   vpc_id = module.vpc.vpc_id
 
 #  tags = local.common_tags
