de7ff5ef4e2d3594914181e61b0c83a5fc2e4e33
prashanth <prashant301k@gmail.com>
2023-05-19 15:48:48 +0530

    security grp

--- security_test/modules/private-sg/main.tf
+++ security_test/modules/private-sg/main.tf
@@ -3,23 +3,22 @@ resource "aws_security_group" "private_sg" {
   description = "Allow inbound traffic"
   vpc_id = var.vpc_id
 
-  ingress = [
-    {
+  ingress {
     description      = "TLS from vpc"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  },
-  {
+  }
+  ingress {
     description = ""
     from_port   = "80"
     to_port     = "80"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ]
+
   egress {
     from_port        = 0
     to_port          = 0
