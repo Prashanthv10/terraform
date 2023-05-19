b01783c89ab67e0db7aa5046374b19905ce1e960
prashanth <prashant301k@gmail.com>
2023-05-19 11:02:52 +0530

    security grp

--- security_test/c5-04-private-securitygrp.tf
+++ security_test/c5-04-private-securitygrp.tf
@@ -4,7 +4,7 @@ module "private-sg" {
   # insert the 2 required variables here
  
   # name = "private-sg"
-  # vpc_id = module.vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
 
 #  tags = local.common_tags
  }
