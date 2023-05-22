83c58807ab8945bc766c4608468c792b17f50d40
prashanth <prashant301k@gmail.com>
2023-05-22 12:47:07 +0530

    loadbalancer

--- security_test_2/c10-classic-lb.tf
+++ security_test_2/c10-classic-lb.tf
@@ -38,10 +38,10 @@ module "elb" {
 
   // ELB attachments
   number_of_instances = 2
-  # instances = { module.ec2-private.id[*] }
-  instances           = [module.ec2-private.id[0],
-   module.ec2-private.id[1]
-   ]
+  instances = "{ module.ec2-private[*].id }"
+  # instances           = [module.ec2-private.id[0],
+  #  module.ec2-private.id[1]
+  #  ]
 
   tags = local.common_tags
 }
