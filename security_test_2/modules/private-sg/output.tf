19216c611ffe9f1c98fb076ec8dcbbd1316b5d63
prashanth <prashant301k@gmail.com>
2023-05-22 16:16:01 +0530

    loadbalancer

--- security_test_2/modules/private-sg/output.tf
+++ security_test_2/modules/private-sg/output.tf
@@ -1,5 +1,5 @@
 output "security_group_id" {
-    value = "aws_security_group.private_sg.id"
+    value = aws_security_group.private_sg.id
 }
 output "security_group_name" {
   description = "The name of the security group"
