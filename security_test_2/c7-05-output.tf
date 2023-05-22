083e1896008d8c8ee387ff1a2d184df8c81484f2
prashanth <prashant301k@gmail.com>
2023-05-22 12:37:09 +0530

    loadbalancer

--- security_test_2/c7-05-output.tf
+++ security_test_2/c7-05-output.tf
@@ -4,9 +4,10 @@ output "ec2-public" {
 output "ec2-public_ip" {
     value = module.ec2-public.public_ip
 }
-output "ec2_bastion_public_instance_ids" {
+# Private EC2 Instances
+output "ec2_private_instance_ids" {
   description = "List of IDs of instances"
-  value       = module.ec2_public.id
+  value       = module.ec2-private.id
 }
 output "ec2-private-private_ip" {
     value = module.ec2-private.private_ip
