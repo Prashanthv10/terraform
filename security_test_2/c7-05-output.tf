c4d06dc256442fd06eef03a4c14d17edda241dc6
prashanth <prashant301k@gmail.com>
2023-05-22 12:30:05 +0530

    loadbalancer

--- security_test_2/c7-05-output.tf
+++ security_test_2/c7-05-output.tf
@@ -5,7 +5,7 @@ output "ec2-public_ip" {
     value = module.ec2-public.public_ip
 }
 output "ec2-private" {
-    value = module.ec2-private.id
+    value = [ module.ec2-private.id[0], module.ec2-private.id[1] ]
 }
 output "ec2-private-private_ip" {
     value = module.ec2-private.private_ip
