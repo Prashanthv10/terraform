d75205fdebf2b109fc1a7147076f0aaab110d758
prashanth <prashant301k@gmail.com>
2023-05-24 11:46:25 +0530

    added application load balancer code

--- security_test_2/c1-version.tf
+++ security_test_2/c1-version.tf
@@ -1,10 +1,10 @@
 terraform {
-  // required_version = "~>0.14.3"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3"
+      version = "4.60.0"
     }
+  
     null = {  
       source = "hashicorp/null"
       version = "3.0.0"
