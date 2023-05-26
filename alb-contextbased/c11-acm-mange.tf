cdae1d12728902123ba00320c35f7003027976e9
prashanth <prashant301k@gmail.com>
2023-05-26 22:00:24 +0530

    context based alb

--- alb-contextbased/c11-acm-mange.tf
+++ alb-contextbased/c11-acm-mange.tf
@@ -1,8 +1,8 @@
 module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "4.3.2"
-  domain_name  = trimsuffix(data.aws_route53_zone.mydomain.zone_id, ".")
-  zone_id      = data.aws_route53_zone.mydomain.zone_id
+  domain_name  = trimsuffix(data.aws_route53_zone.mydomain.id, ".")
+  zone_id      = data.aws_route53_zone.mydomain.id
 
   subject_alternative_names = [
     "*.testdevopsincloud.net"
