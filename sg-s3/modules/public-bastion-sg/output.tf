006c7554ce231d6587cb56012edb98cc982121f0
prashanth <prashant301k@gmail.com>
2023-06-01 15:53:53 +0530

    added remote state

--- sg-s3/modules/public-bastion-sg/output.tf
+++ sg-s3/modules/public-bastion-sg/output.tf
@@ -5,3 +5,6 @@ output "security_group_name" {
   description = "The name of the security group"
   value       = aws_security_group.bastion_sg.name
 }
+output "vpc_id" {
+  value = data.terraform_remote_state.vpc.outputs.vpc_id
+}
