c370812bb93836a93666acf92a5ba9b23bed5dd0
prashanth <prashant301k@gmail.com>
2023-06-01 16:02:12 +0530

    added remote state

---
+++ sg-s3/c0-vpcprjct-tfstate.tf
@@ -0,0 +1,10 @@
+# data "terraform_remote_state" "vpc" {
+#   backend = "s3"
+#   config = {
+#     bucket = "state-bucket-prash"
+#     key = "dev/project1-vpc/terraform.tfstate"
+#     region = "ap-southeast-2"
+
+#     }
+#   }
+
--- sg-s3/c7-05-output.tf
+++ sg-s3/c7-05-output.tf
@@ -1,7 +1,7 @@
-output "ec2-public" {
-    value = module.ec2-public.id
-}
-output "ec2-public_ip" {
-    value = module.ec2-public.public_ip
-}
+# output "ec2-public" {
+#     value = module.ec2-public.id
+# }
+# output "ec2-public_ip" {
+#     value = module.ec2-public.public_ip
+# }
 
