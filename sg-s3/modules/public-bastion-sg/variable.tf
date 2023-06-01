901629a8d7f4dd66a0f6772de842ca6ce6b6863f
prashanth <prashant301k@gmail.com>
2023-06-01 22:25:03 +0530

    added remote state

--- sg-s3/modules/public-bastion-sg/variable.tf
+++ sg-s3/modules/public-bastion-sg/variable.tf
@@ -1,3 +1,1 @@
-variable "vpc_id" {
-  dedefault = data.terraform_remote_state.vpc.outputs.vpc_id
-    }
+variable "vpc_id"  {}
---
+++ sg-s3/variable.tf
@@ -0,0 +1,3 @@
+variable "vpc_id" {
+  default = data.terraform_remote_state.vpc.outputs.vpc_id
+}
