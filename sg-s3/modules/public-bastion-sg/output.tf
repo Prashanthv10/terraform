0a1643abdcbc1285fca8097537e8d6d01bc8aa7a
prashanth <prashant301k@gmail.com>
2023-06-02 13:17:11 +0530

    added

--- sg-s3/modules/public-bastion-sg/output.tf
+++ sg-s3/modules/public-bastion-sg/output.tf
@@ -1,10 +1,10 @@
-output "security_group_id" {
-    value = aws_security_group.bastion_sg.id
-}
-output "security_group_name" {
-  description = "The name of the security group"
-  value       = aws_security_group.bastion_sg.name
-}
-# output "vpc_id" {
-#   value = data.terraform_remote_state.vpc.outputs.vpc_id
+# output "security_group_id" {
+#     value = aws_security_group.bastion_sg.id
 # }
+# output "security_group_name" {
+#   description = "The name of the security group"
+#   value       = aws_security_group.bastion_sg.name
+# }
+# # output "vpc_id" {
+# #   value = data.terraform_remote_state.vpc.outputs.vpc_id
+# # }
